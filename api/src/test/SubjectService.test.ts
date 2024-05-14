92bb29d8913a3a4946b11c602a33d1b2fccc5fce
gmiserani <gabrielamiserani@uol.com.br>
2024-05-14 20:03:47 -0300

    subject new tests

--- api/src/test/SubjectService.test.ts
+++ api/src/test/SubjectService.test.ts
@@ -146,3 +146,253 @@ describe("getById", () => {
         await expect(SubjectService.getById("1")).rejects.toThrow("Subject not found");
     });
 });
+
+describe("getAllSummaryNormalOrder", () => {
+    let findManySubjects: Subject[];
+
+    beforeEach(() => {
+        vi.resetAllMocks();
+
+        findManySubjects = [
+            {
+                id: "1",
+                name: "test",
+                syllabus: "test",
+                mode: "test",
+                date: "Mon/Wed",
+                time: "17:00",
+                semester: 1,
+                workload: 1,
+                teacherId: "1",
+            },
+            {
+                id: "2",
+                name: "test",
+                syllabus: "test",
+                mode: "test",
+                date: "Mon/Wed",
+                time: "17:00",
+                semester: 1,
+                workload: 1,
+                teacherId: "1",
+            },
+        ];
+    });
+
+    test("Should call findMany with select options", async () => {
+        prisma.subject.findMany.mockResolvedValueOnce(findManySubjects);
+
+        await SubjectService.getAllSummaryNormalOrder();
+
+        expect(prisma.subject.findMany).toHaveBeenCalledWith({
+            select: {
+                id: true,
+                name: true,
+                overall_rating: true,
+            },
+        });
+    });
+    test("Should return all subjects", async () => {
+        prisma.subject.findMany.mockResolvedValueOnce(findManySubjects);
+
+        const subjects = await SubjectService.getAllSummaryNormalOrder();
+
+        expect(subjects).toEqual(findManySubjects);
+    });
+
+    test("Should throw error if no subjects are found", async () => {
+        prisma.subject.findMany.mockResolvedValueOnce();
+
+        await expect(SubjectService.getAllSummaryNormalOrder()).rejects.toThrow("No subjects found");
+    });
+
+});
+
+describe("getAllSummaryRatingOrderDesc", () => {
+    let findManySubjects: Subject[];
+
+    beforeEach(() => {
+        vi.resetAllMocks();
+
+        findManySubjects = [
+            {
+                id: "1",
+                name: "test",
+                syllabus: "test",
+                mode: "test",
+                date: "Mon/Wed",
+                time: "17:00",
+                semester: 1,
+                workload: 1,
+                teacherId: "1",
+                overall_rating: 5,
+            },
+            {
+                id: "2",
+                name: "test",
+                syllabus: "test",
+                mode: "test",
+                date: "Mon/Wed",
+                time: "17:00",
+                semester: 1,
+                workload: 1,
+                teacherId: "1",
+                overall_rating: 4,
+            },
+        ];
+    });
+
+    test("Should call findMany with select options", async () => {
+        prisma.subject.findMany.mockResolvedValueOnce(findManySubjects);
+
+        await SubjectService.getAllSummaryRatingOrderDesc();
+
+        expect(prisma.subject.findMany).toHaveBeenCalledWith({
+            select: {
+                id: true,
+                name: true,
+                overall_rating: true,
+            },
+            orderBy: {
+                overall_rating: "desc",
+            },
+        });
+    });
+
+    test("Should return all subjects", async () => {
+        prisma.subject.findMany.mockResolvedValueOnce(findManySubjects);
+
+        const subjects = await SubjectService.getAllSummaryRatingOrderDesc();
+
+        expect(subjects).toEqual(findManySubjects);
+    });
+
+    test("Should throw error if no subjects are found", async () => {
+        prisma.subject.findMany.mockResolvedValueOnce();
+
+        await expect(SubjectService.getAllSummaryRatingOrderDesc()).rejects.toThrow("No subjects found");
+    });
+});
+
+describe("getAllSummaryRatingOrderAsc", () => {
+    let findManySubjects: Subject[];
+
+    beforeEach(() => {
+        vi.resetAllMocks();
+
+        findManySubjects = [
+            {
+                id: "1",
+                name: "test",
+                syllabus: "test",
+                mode: "test",
+                date: "Mon/Wed",
+                time: "17:00",
+                semester: 1,
+                workload: 1,
+                teacherId: "1",
+                overall_rating: 4,
+            },
+            {
+                id: "2",
+                name: "test",
+                syllabus: "test",
+                mode: "test",
+                date: "Mon/Wed",
+                time: "17:00",
+                semester: 1,
+                workload: 1,
+                teacherId: "1",
+                overall_rating: 5,
+            },
+        ];
+    });
+
+    test("Should call findMany with select options", async () => {
+        prisma.subject.findMany.mockResolvedValueOnce(findManySubjects);
+
+        await SubjectService.getAllSummaryRatingOrderAsc();
+
+        expect(prisma.subject.findMany).toHaveBeenCalledWith({
+            select: {
+                id: true,
+                name: true,
+                overall_rating: true,
+            },
+            orderBy: {
+                overall_rating: "asc",
+            },
+        });
+    });
+
+    test("Should return all subjects", async () => {
+        prisma.subject.findMany.mockResolvedValueOnce(findManySubjects);
+
+        const subjects = await SubjectService.getAllSummaryRatingOrderAsc();
+
+        expect(subjects).toEqual(findManySubjects);
+    });
+
+    test("Should throw error if no subjects are found", async () => {
+        prisma.subject.findMany.mockResolvedValueOnce();
+
+        await expect(SubjectService.getAllSummaryRatingOrderAsc()).rejects.toThrow("No subjects found");
+    });
+});
+
+describe("getReviews", () => {
+    let findFirstSubject: Subject;
+
+    beforeEach(() => {
+        vi.resetAllMocks();
+
+        findFirstSubject = {
+            id: "1",
+            name: "test",
+            syllabus: "test",
+            mode: "test",
+            date: "Mon/Wed",
+            time: "17:00",
+            semester: 1,
+            workload: 1,
+            teacherId: "1",
+        };
+    });
+
+    test("Should call findFirst with subject id", async () => {
+        prisma.subject.findFirst.mockResolvedValueOnce(findFirstSubject);
+
+        await SubjectService.getReviews("1");
+
+        expect(prisma.subject.findFirst).toHaveBeenCalledWith({
+            where: {
+                id: "1",
+            },
+            select: {
+                reviews: {
+                    select: {
+                        user: {
+                            select: {
+                                username: true,
+                            },
+                        },
+                        test_rating: true,
+                        project_rating: true,
+                        teacher_rating: true,
+                        effort_rating: true,
+                        presence_rating: true,
+                        overall_rating: true,
+                        comment: true,
+                    },
+                },
+            },
+        });
+    });
+
+    test("Should throw error if subject is not found", async () => {
+        prisma.subject.findFirst.mockResolvedValueOnce(null);
+
+        await expect(SubjectService.getReviews("1")).rejects.toThrow("Subject not found");
+    });
+});
+            
