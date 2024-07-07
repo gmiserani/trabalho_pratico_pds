924b23c9b2f99ff8d1ef6f442e8d664d00186f4f
marialuiza <maria_luiza1598@hotmail.com>
2024-07-07 17:34:29 -0300

    separating e2e tests

--- client/cypress/e2e/e2e.cy.js
+++ client/cypress/e2e/e2e.cy.js
@@ -2,50 +2,13 @@
 /// <reference types="cypress" />
 
 
-describe('E2E tests', () => {
+describe('Login', () => {
     const random = Math.floor(Math.random() * 100000);
 
     beforeEach(() => {
         cy.visit('http://localhost:5173/');
         cy.wait(2000);
         cy.clearAllCookies();
-    });
-
-    it('should be in login url', () => {
-        cy.url().should('eq', 'http://localhost:5173/login');
-    });
-
-    it('should displays input fields', () => {
-        cy.get('.text-input').should('have.length', 2);
-    });
-
-    it('should go to sign up page', () => {
-        cy.get('.signup').click();
-        cy.url().should('include', '/signup');
-    });
-
-    it('should fill in sign up form', () => {
-        cy.get('.signup').click();
-        cy.wait(2000);
-        cy.get('.text-input').eq(0).type('test');
-        cy.get('.text-input').eq(1).type('test' + random);
-        cy.get('.text-input').eq(2).type('test' + random + '@test.com');
-        cy.get('.text-input').eq(3).type('Ciencia da computacao');
-        cy.get('.text-input').eq(4).type('1');
-        cy.get('.text-input').eq(5).type('test123');
-        cy.get('.entrar').click();
-        cy.url().should('eq', 'http://localhost:5173/');
-    });
-
-    it('should login and go a subject page when clicking on suvjects list item', () => {
-        cy.get('.text-input').eq(0).type('test' + random + '@test.com');
-        cy.get('.text-input').eq(1).type('test123');
-        cy.get('.entrar').click();
-        cy.wait(1000);
-        cy.url().should('eq', 'http://localhost:5173/');
-        cy.wait(1000);
-        cy.get('.subjectMiniature').eq(0).click();
-        cy.url().should('include', '/subject/');
     });
 
     it('should login and logout', () => {
@@ -57,5 +20,47 @@ describe('E2E tests', () => {
         cy.wait(1000);
         cy.get('.header').find('.logoutLink').click();
         cy.url().should('eq', 'http://localhost:5173/login');
+    });
+});
+
+describe('Sign up', () => {
+    const random = Math.floor(Math.random() * 100000);
+
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/');
+        cy.wait(2000);
+        cy.clearAllCookies();
+    });
+
+    it('should fill in sign up form and create account', () => {
+        cy.get('.signup').click();
+        cy.wait(2000);
+        cy.get('.text-input').eq(0).type('test');
+        cy.get('.text-input').eq(1).type('test' + random);
+        cy.get('.text-input').eq(2).type('test' + random + '@test.com');
+        cy.get('.text-input').eq(3).type('Ciencia da computacao');
+        cy.get('.text-input').eq(4).type('1');
+        cy.get('.text-input').eq(5).type('test123');
+        cy.get('.entrar').click();
+        cy.url().should('eq', 'http://localhost:5173/');
+    });
+});
+
+describe('Subject-page', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/');
+        cy.wait(2000);
+        cy.clearAllCookies();
+    });
+
+    it('should login and go a subject page when clicking on suvjects list item', () => {
+        cy.get('.text-input').eq(0).type('test' + random + '@test.com');
+        cy.get('.text-input').eq(1).type('test123');
+        cy.get('.entrar').click();
+        cy.wait(1000);
+        cy.url().should('eq', 'http://localhost:5173/');
+        cy.wait(1000);
+        cy.get('.subjectMiniature').eq(0).click();
+        cy.url().should('include', '/subject/');
     });
 });
