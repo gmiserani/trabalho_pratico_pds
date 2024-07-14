0e122a214f84517ee415c4dc1ff9de6f572a4158
raissamiranda <raissamm01@hotmail.com>
2024-07-14 14:18:14 -0300

    adding e2e test

--- client/cypress/e2e/e2e.cy.js
+++ client/cypress/e2e/e2e.cy.js
@@ -52,7 +52,7 @@ describe('Subject-page', () => {
         cy.clearAllCookies();
     });
 
-    it('should login and go a subject page when clicking on suvjects list item', () => {
+    it('should login and go a subject page when clicking on subjects list item', () => {
         cy.get('.text-input').eq(0).type('bella@prisma.io');
         cy.get('.text-input').eq(1).type('123');
         cy.get('.entrar').click();
@@ -63,3 +63,24 @@ describe('Subject-page', () => {
         cy.url().should('include', '/subject/');
     });
 });
+
+describe('Evaluation-page', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/');
+        cy.wait(2000);
+        cy.clearAllCookies();
+    });
+
+    it('should login and go a evaluation page when clicking on evaluations list item', () => {
+        cy.get('.text-input').eq(0).type('bella@prisma.io');
+        cy.get('.text-input').eq(1).type('123');
+        cy.get('.entrar').click();
+        cy.wait(1000);
+        cy.url().should('eq', 'http://localhost:5173/'); //confirma que a url permanece a mesma após o login
+        cy.wait(1000);
+        // clicar em uma disciplina antes?
+        cy.get('.evaluationMiniature').eq(0).click();
+        cy.url().should('include', '/evaluation/'); //confirma que a url mudou para a página de avaliação
+    } );
+});
+
